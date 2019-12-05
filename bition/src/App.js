a2d38907322c216ed08d0f58fc664dbf5433066c
Fermenting <realferalxanderyell@gmail.com>
2019-12-04 20:59:11 -0800

    completion of Part I: Setting Up The Project

--- bition/src/App.js
+++ bition/src/App.js
@@ -4,20 +4,40 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component"
 import TodosList from "./components/todos-list.component"
+import logo from "./logo.png"
 
 
 function App() {
   return (
+    
     <Router>
-   <div className="container">
-     <h2>MERN To Do / Prohibitor APP
-     </h2>
-   </div>
-   <Route path="/" exact component={TodosList} />
-   <Route path="/edit/:id" component={EditTodo} />
-   <Route path="/create" component={CreateTodo} />
-   </Router>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="https://fermenting.github.io" target="_blank" rel="noopener noreferrer">
+          <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+        </a>
+        <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+        <div className="collpase navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">Todos</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">Create Todo</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <br />
+
+
+      <Route path="/" exact component={TodosList} />
+      <Route path="/edit/:id" component={EditTodo} />
+      <Route path="/create" component={CreateTodo} />
+    </div>
+    </Router >
   );
 }
 
 export default App;
+
