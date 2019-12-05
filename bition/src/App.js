f5e8347dfd51d1f31278bb7f05626e837f9916b1
Fermenting <realferalxanderyell@gmail.com>
2019-12-04 16:13:24 -0800

    3 basic components

--- bition/src/App.js
+++ bition/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component"
+import TodosList from "./components/todos-list.component"
+
 
 function App() {
   return (
+    <Router>
    <div className="container">
      <h2>MERN To Do / Prohibitor APP
      </h2>
    </div>
+   <Route path="/" exact component={TodosList} />
+   <Route path="/edit/:id" component={EditTodo} />
+   <Route path="/create" component={CreateTodo} />
+   </Router>
   );
 }
 
---
+++ bition/src/components/create-todo.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class TodosList extends Component {
+  render () {
+    return (
+      <div>
+        <p>Welecome to Create Todo Component</p>
+      </div>
+    )
+  }
+}
